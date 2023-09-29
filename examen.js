c658843165c9421568e2da295577617c646e0c4e
Pace Leandro <93685034+PaceLeandro@users.noreply.github.com>
2023-09-29 19:39:12 -0300

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,98 @@
+const http = require('http')
+const fs = require('fs')
+
+const mime = {
+  'html': 'text/html',
+  'css': 'text/css',
+  'jpg': 'image/jpg',
+  'ico': 'image/x-icon',
+  'mp3': 'audio/mpeg3',
+  'mp4': 'video/mp4'
+}
+
+const servidor = http.createServer((pedido, respuesta) => {
+  const url = new URL('http://localhost:8888' + pedido.url)
+  let camino = 'public' + url.pathname
+  if (camino == 'public/')
+    camino = 'public/index.html'
+  encaminar(pedido, respuesta, camino)
+})
+
+servidor.listen(8888)
+
+
+function encaminar(pedido, respuesta, camino) {
+  console.log(camino)
+  switch (camino) {
+    case 'public/recuperardatos': {
+      recuperar(pedido, respuesta)
+      break
+    }
+    default: {
+      fs.stat(camino, error => {
+        if (!error) {
+          fs.readFile(camino, (error, contenido) => {
+            if (error) {
+              respuesta.writeHead(500, { 'Content-Type': 'text/plain' })
+              respuesta.write('Error interno')
+              respuesta.end()
+            } else {
+              const vec = camino.split('.')
+              const extension = vec[vec.length - 1]
+              const mimearchivo = mime[extension]
+              respuesta.writeHead(200, { 'Content-Type': mimearchivo })
+              respuesta.write(contenido)
+              respuesta.end()
+            }
+          })
+        } else {
+          respuesta.writeHead(404, { 'Content-Type': 'text/html' })
+          respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>')
+          respuesta.end()
+        }
+      })
+    }
+  }
+}
+
+
+function recuperar(pedido, respuesta) {
+  let info = ''
+  pedido.on('data', datosparciales => {
+    info += datosparciales
+  })
+  pedido.on('end', () => {
+    const formulario = new URLSearchParams(info)
+    console.log(formulario)
+    respuesta.writeHead(200, { 'Content-Type': 'text/html' })
+    let x=formulario.get('clave');
+      const pagina =
+        `<!doctype html><html><head></head><body>
+        Clave:${change(x)}<br>
+        <a href="index.html">Retornar</a>
+        </body></html>`
+    respuesta.end(pagina)
+  })
+}
+
+function change(palabra){
+    let vowels=['a','e','i','o','u','A','E','I','O','U']
+    palabra.toString();
+    let traduction='';
+    
+    for(let i=0;i<palabra.length;i++){
+      
+        if(palabra[i]==' '){
+        
+        }        
+        if(vowels.includes(palabra[i])==false){
+            traduction+=palabra[i];
+        }
+        if(vowels.includes(palabra[i])){
+            traduction += palabra[i] + 'p' +palabra[i];
+        }
+    }
+    return traduction;
+}
+
+console.log('Servidor web iniciado')
